3129ce3becdf71a12a6b09cc442863405733694c
Hitesh Sahu <hiteshkrsahu@gmail.com>
2023-12-10 19:44:53 +0100

    Improved Tenant Workflow
    - Improved Tenant list by showing more info
    - Added tooltip to show action of FAB
    - Added snackbar popup to indicate successful deletion of tenant
    - Improved dialog to add new tenant
    - Added progressbar to show loading of tenant screen
    - Added progressbar to show deletion of tenant
    - Added progressbar in dialog to show adding new user
    - Added form validation in create used dialog
    - Fixed bug with deletion of tenant
    -

--- angular/src/app/tenants/tenants.component.ts
+++ angular/src/app/tenants/tenants.component.ts
@@ -1,33 +1,57 @@
-import {ChangeDetectorRef, Component, inject} from '@angular/core';
-import {HttpClient} from "@angular/common/http";
-import {MatDialog} from "@angular/material/dialog";
-import {AddTenantComponent} from "../add-tenant/add-tenant.component";
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, inject } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { MatDialog } from "@angular/material/dialog";
+import { AddTenantComponent } from "../add-tenant/add-tenant.component";
 import { environment } from '../../environments/environment';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-tenants',
   templateUrl: './tenants.component.html',
-  styleUrls: ['./tenants.component.scss']
+  styleUrls: ['./tenants.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+
 })
 export class TenantsComponent {
   public tenants: any
   private http = inject(HttpClient)
+  public showProgressbar = false
 
-  constructor(http: HttpClient, public dialog: MatDialog) {
-    http.get(`${environment.apiURL}/show-tenants`).subscribe((tenants) => {
-      this.tenants = tenants;
-    })
+  constructor(private cd: ChangeDetectorRef, public dialog: MatDialog, private snackBar: MatSnackBar) { }
+
+  async ngOnInit() {
+    this.loadTanent()
   }
 
+  loadTanent() {
+    this.showProgressbar= true
+    this.http.get(`${environment.apiURL}/show-tenants`).subscribe((tenants) => {
+      this.tenants = tenants;
+      this.showProgressbar= false
+      this.cd.detectChanges();
+    })
+  }
 
   addTenant() {
     this.dialog.open(AddTenantComponent);
+    this.dialog.afterAllClosed.subscribe(result => {
+      this.loadTanent()
+    });
   }
 
   deleteTenant(tenant: any) {
-    this.tenants.splice(this.tenants.indexOf(tenant), 1);
-    this.http.post(`${environment.apiURL}/delete-tenant?name=` + tenant.name, {}).subscribe((res) => {
-      console.log(res);
+    this.showProgressbar= true
+    //  this.tenants.splice(this.tenants.indexOf(tenant), 1);
+    this.http.post(`${environment.apiURL}/delete-tenant?name=` + tenant.name, null).subscribe((res) => {
+      this.showSnackBar("Deleted Tenant " + tenant.name)
+      this.showProgressbar= false
+      this.loadTanent()
     })
   }
+
+  showSnackBar(message: string) {
+    this.snackBar.open(message);
+  }
 }
+
+
