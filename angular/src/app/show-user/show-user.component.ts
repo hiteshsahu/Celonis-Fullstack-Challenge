a1ba976f87bd726c6286a486838e64621d10b085
Hitesh Sahu <hiteshkrsahu@gmail.com>
2023-12-11 13:15:58 +0100

    Improve user screen with more info

--- angular/src/app/show-user/show-user.component.ts
+++ angular/src/app/show-user/show-user.component.ts
@@ -1,15 +1,17 @@
-import {Component, inject, Input} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {HttpClient} from "@angular/common/http";
-import {MatButtonModule} from "@angular/material/button";
-import {MatInputModule} from "@angular/material/input";
-import {FormsModule} from "@angular/forms";
+import { Component, inject, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from "@angular/common/http";
+import { MatButtonModule } from "@angular/material/button";
+import { MatInputModule } from "@angular/material/input";
+import { FormsModule } from "@angular/forms";
 import { environment } from '../../environments/environment';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-show-user',
   standalone: true,
-  imports: [CommonModule, MatButtonModule, MatInputModule, FormsModule],
+  imports: [CommonModule, MatButtonModule, MatInputModule, FormsModule, MatIconModule],
   templateUrl: './show-user.component.html',
   styleUrls: ['./show-user.component.scss']
 })
@@ -17,26 +19,40 @@ export class ShowUserComponent {
   public user: any;
   @Input() id: any;
   public editMode: boolean = false;
-  public name: any
+  public name: string = ""
   private http = inject(HttpClient)
 
-  constructor(http: HttpClient) {
+  constructor(http: HttpClient, private snackBar: MatSnackBar) {
     setTimeout(() => {
-      http.get(`${environment.apiURL}/show-user/` + this.id).subscribe((user:any) => {
-        this.user = user;
-        this.name = user.name;
-      });
+      this.showUser();
     }, 100)
   }
 
-  save() {
+  editUser() {
+    this.editMode = true;
+  }
+
+  saveUser() {
     this.editMode = false;
-    this.http.put(`${environment.apiURL}/update-user/` + this.id + '?name=' + this.name, null).subscribe((res) => {
-      console.log(res);
-    });
-    this.http.get(`${environment.apiURL}/show-user/` + this.id).subscribe((user:any) => {
+    this.updateUser()
+  }
+
+  showUser() {
+    this.http.get(`${environment.apiURL}/show-user/` + this.id).subscribe((user: any) => {
       this.user = user;
       this.name = user.name;
     });
+  }
+
+  updateUser() {
+    this.http.put(`${environment.apiURL}/update-user/` + this.id + '?name=' + this.name, null).subscribe((res) => {
+      console.log(res);
+      this.showSnackBar("User Updated!")
+      this.showUser();
+    });
+  }
+
+  showSnackBar(message: string) {
+    this.snackBar.open(message);
   }
 }
