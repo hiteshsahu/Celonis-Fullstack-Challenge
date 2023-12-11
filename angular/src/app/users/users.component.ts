3eb5539e32e44d0a3d76fdf13af3c83e7fb04fd3
Hitesh Sahu <hiteshkrsahu@gmail.com>
2023-12-11 23:11:29 +0100

    Final touch up
    - Added types in server side
    - Added Page headings
    - Added dummy property images
    - Updated DB seed with more data Items
    - Extracted common styles

--- angular/src/app/users/users.component.ts
+++ angular/src/app/users/users.component.ts
@@ -14,6 +14,17 @@ export class UsersComponent implements OnInit {
   public users: any
   private dialog = inject(MatDialog)
   public showProgressbar = false
+  private nextPropety = 0
+  private rentalProperties = [
+    "https://images.pexels.com/photos/186077/pexels-photo-186077.jpeg?auto=compress&cs=tinysrgb&w=600",
+    "https://images.pexels.com/photos/259588/pexels-photo-259588.jpeg?auto=compress&cs=tinysrgb&w=600",
+    "https://images.pexels.com/photos/1438832/pexels-photo-1438832.jpeg?auto=compress&cs=tinysrgb&w=600",
+    "https://images.pexels.com/photos/2079234/pexels-photo-2079234.jpeg?auto=compress&cs=tinysrgb&w=600",
+    "https://images.pexels.com/photos/2462015/pexels-photo-2462015.jpeg?auto=compress&cs=tinysrgb&w=600",
+    "https://images.pexels.com/photos/65438/pexels-photo-65438.jpeg?auto=compress&cs=tinysrgb&w=600",
+    "https://images.pexels.com/photos/1500459/pexels-photo-1500459.jpeg?auto=compress&cs=tinysrgb&w=600",
+    "https://images.pexels.com/photos/259751/pexels-photo-259751.jpeg?auto=compress&cs=tinysrgb&w=600"]
+
 
   constructor(private http: HttpClient, private cd: ChangeDetectorRef) { }
 
@@ -23,11 +34,13 @@ export class UsersComponent implements OnInit {
 
   loadUsers() {
     this.showProgressbar = true
+    this.nextPropety = 0
     this.http.get(`${environment.apiURL}/list-users`).subscribe((users) => {
       this.users = users;
       this.users.forEach((user: any) => {
         this.http.get(`${environment.apiURL}/send-user-tenant/` + user.email).subscribe((tenant) => {
           user.tenant = tenant;
+          user.rentalImage = this.getHouseImage();
           this.showProgressbar = false
           this.cd.detectChanges();
         })
@@ -49,5 +62,16 @@ export class UsersComponent implements OnInit {
       this.showProgressbar = false
       this.cd.detectChanges();
     })
+  }
+
+
+  getHouseImage = () => {
+    const rentalProperty = this.rentalProperties[this.nextPropety]
+    if (this.nextPropety >= this.rentalProperties.length) {
+      this.nextPropety = 0
+    } else {
+      this.nextPropety++
+    }
+    return rentalProperty
   }
 }
